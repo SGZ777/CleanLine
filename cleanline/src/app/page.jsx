fdeff5c336974ee995e553667250f653be5c38af
SGZ777 <arthursgz77@gmail.com>
2026-03-20 16:38:59 -0300

    feat: add Inter font and update layout language to Portuguese; enhance Header component styling

--- cleanline/src/app/page.jsx
+++ cleanline/src/app/page.jsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <body>
         <Header></Header>
-
+        <h1 className="mt-10 ms-5 text-7xl">Auditoria <span className="text-[#24bff6]">5S</span> com confirmação física</h1>
       </body>
     </>
   );
