3dd3b90e79ea43205c3bffb29c93440853aca630
Mat-fugi <souzafugikawamateus@gmail.com>
2026-03-20 17:00:23 -0300

    "adiciona scripts 01..12 para criar tabelas Adm, Supervisor, Func_Limpeza, Rota, Setor, Vistoria, etc.configura chaves primárias, estrangeiras e tipos de dados iniciais."

---
+++ cleanline/src/lib/db/migrations/09-2026-03-20_create_table_Equipe_Limpeza.sql
@@ -0,0 +1,10 @@
+-- Criando a tabela Equipe_Limpeza
+-- Data: 20/03/2026
+
+
+USE CleanLine;
+
+CREATE TABLE `Equipe_Limpeza`(
+    `Id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Nome` VARCHAR(255) NOT NULL
+);
