ee27be798c0e3763e0fa7a64ac7e1dc6b66cb03d
SGZ777 <arthursgz77@gmail.com>
2026-03-20 17:14:22 -0300

    feat: update page layout with enhanced heading and description for 5S audit

--- cleanline/src/app/page.jsx
+++ cleanline/src/app/page.jsx
@@ -7,7 +7,9 @@ export default function Home() {
     <>
       <body>
         <Header></Header>
-        <h1 className="mt-10 ms-5 text-7xl">Auditoria <span className="text-[#24bff6]">5S</span> com confirmação física</h1>
+        <h1 className="mt-10 ms-5 text-8xl p-[3vw]">Auditoria <span className="text-[#24bff6]">5S</span> com<br/> confirmação física</h1>
+        <p className="text-3xl ms-5 ps-[3vw]">O CleanLine garante que as auditorias 5S sejam<br/> realizadas no local correto, com comprovação real<br/> das condições do ambiente.</p>
+        <button>Fazer Login</button>
       </body>
     </>
   );
