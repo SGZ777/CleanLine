042e262afe6d9d2b5acec575eab5eb1f67e861d4
miguelvkz <miguelguedeslarios@gmail.com>
2026-03-20 17:07:52 -0300

    feat: implement JWT authentication and update Login1 component for user login

--- cleanline/src/app/api/auth/route.js
+++ cleanline/src/app/api/auth/route.js
@@ -1,40 +1,37 @@
 import { NextResponse } from 'next/server';
 import fs from 'fs/promises';
 import path from 'path';
+import jwt from 'jsonwebtoken'; // <<< Importamos o JWT
+
+// Essa chave secreta idealmente ficaria no seu .env.local (ex: JWT_SECRET=suachave)
+const SECRET_KEY = process.env.JWT_SECRET 
 
 export async function POST(request) {
   try {
-   
-    const body = await request.json();
-    const { email, senha } = body;
-
-   
-    const filePath = path.join(process.cwd(), 'src', 'lib', 'db', 'users.json');
+    const { email, senha } = await request.json();
     
-    //transforma em json
+    const filePath = path.join(process.cwd(), 'src', 'lib', 'db', 'users.json');
     const fileData = await fs.readFile(filePath, 'utf-8');
     const db = JSON.parse(fileData);
 
-   
     const user = db.users.find(u => u.email === email && u.senha === senha);
 
-    
     if (!user) {
       return NextResponse.json({ erro: 'Email ou senha incorretos' }, { status: 401 });
     }
 
-    
-    return NextResponse.json({ 
-      mensagem: 'Login feito com sucesso!', 
-      user: { 
-        id: user.id, 
-        nome: user.nome, 
-        cargo: user.cargo
-      } 
-    }, { status: 200 });
+    // <<< A MÁGICA DO JWT ACONTECE AQUI >>>
+    // Criamos o token com o ID e a Role do cara, válido por 8 horas
+    const token = jwt.sign(
+      { id: user.id, role: user.role }, 
+      SECRET_KEY, 
+      { expiresIn: '8h' }
+    );
+
+    // De
+    return NextResponse.json({ mensagem: 'Sucesso', token }, { status: 200 });
 
   } catch (error) {
-    console.error("Erro na rota de login:", error);
-    return NextResponse.json({ erro: 'Erro interno no servidor' }, { status: 500 });
+    return NextResponse.json({ erro: 'Erro no servidor' }, { status: 500 });
   }
 }
