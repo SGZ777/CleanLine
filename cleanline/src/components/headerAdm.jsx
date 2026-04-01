064eb0ccc42e2b59d93822cd326a1caed7cb625a
SGZ777 <arthursgz77@gmail.com>
2026-04-01 17:21:10 -0300

    Remove unnecessary padding from header image in HeaderAdm component

--- cleanline/src/components/headerAdm.jsx
+++ cleanline/src/components/headerAdm.jsx
@@ -3,7 +3,7 @@ export default function HeaderAdm(){
         <header className="w-auto h-auto flex items-center justify-between bg-white p-4">
             <img
             src="logoCleanline.png"
-            className="w-80 lg:ps-15 sm:ps-0 pt-5"
+            className="w-80 lg:ps-15 sm:ps-0"
             alt="Cleanline"
           />
           <p className="text-center text-2xl text-[#0d005d]">Área de administrador</p>
