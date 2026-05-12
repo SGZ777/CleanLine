2aa212ee9330c1924b5a53fe0af76ce37bd35426
SGZ777 <arthursgz77@gmail.com>
2026-05-12 17:04:04 -0300

    update port server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ import inspecoesRoutes from './routes/inspecoes.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
