694a10aad33b49b3595d8bfd6fa47a0eff9a30e2
SGZ777 <arthursgz77@gmail.com>
2026-05-12 16:06:29 -0300

    update equipestable

--- frontend/src/components/equipes/EquipesTable.jsx
+++ frontend/src/components/equipes/EquipesTable.jsx
@@ -285,7 +285,7 @@ export default function EquipesTable({ searchTerm = "" }) {
           <TableRow className="border-b hover:bg-transparent">
             <TableHead className="h-12 px-6 font-medium">Nome</TableHead>
             <TableHead className="h-12 px-6 text-right font-medium pe-20">
-              Acoes
+              Ações
             </TableHead>
           </TableRow>
         </TableHeader>
