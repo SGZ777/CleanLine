c199a4cb4d912fc12aee9141048d5dcc75ecd174
mat-fugi <souzafugikawamateus@gmail.com>
2026-05-15 14:30:21 -0300

    Fazendo alterações para adicionar a funcionalidade de login via mobile. Adicionei a rota GET /login que chama a função loginMobile no authController. Essa função irá lidar com o processo de autenticação para usuários que estão tentando fazer login através de dispositivos móveis.

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { getUser, login, logout } from '../controllers/authController.js';
+import { getUser, login, logout, loginMobile } from '../controllers/authController.js';
 import { authMiddleware } from '../middlewares/auth.js';
 
 const router = Router();
 
 router.post('/auth', login);
 router.post('/logout', logout);
+router.get('/login', loginMobile);
 router.get('/user', authMiddleware, getUser);
 
 export default router;
