9779e762428a6ec7664f389ae35c54a625fa3324
mat-fugi <souzafugikawamateus@gmail.com>
2026-05-18 17:12:21 -0300

    Dando permissão para imagem ser enviada do app para o backend, utilizando multer com memoryStorage para evitar problemas de permissão de escrita no servidor. Além disso, adicionei a configuração do Cloudinary para armazenar as imagens na nuvem, garantindo que elas possam ser acessadas facilmente pelo frontend. No frontend, ajustei a lógica para exibir a imagem correta, verificando se a URL é válida antes de usar um placeholder.

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**', // Permite qualquer imagem do Cloudinary
+      },
+    ],
+  },
 };
 
-
-export default nextConfig;
+module.exports = nextConfig;
